a528da33d7c50b26f9bd5801c2042dfbaf34807a
Mark Borazio <markborazio@hotmail.com>
2022-10-09 23:52:00 +1100

    Initial Commit

---
+++ Shared/SlideOutMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideOutMenuApp.swift
+//  Shared
+//
+//  Created by Mark Borazio [Personal] on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SlideOutMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
